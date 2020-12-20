ca7b1646df0df2839fc5839d429997722dbd33a1
Yiran Li <yiran.li@hotmail.com>
2020-12-20 16:09:15 -0700

    Add handleChange and calculateWordsCount methods

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [text, getText] = useState('');
+  const handleChange = (event) => {
+    getText(event.target.value)
+  }
+
+  const calculateWordsCount = (text) => {
+    const words = text.trim().split(' ');
+    return words.length
+  }
+
   return (
     <section>
       <h1> How fast do you type?</h1>
-      <textarea />
+      <textarea value={text} onChange={handleChange} />
       <h4>Time remaining: </h4>
-      <button>START</button>
-      <h1>Word Count:</h1>
+      <button onClick={() => calculateWordsCount(text)}>START</button>
+      <h1>Word Count:{calculateWordsCount(text)}</h1>
     </section>
   );
 }
