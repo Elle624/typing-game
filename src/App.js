9e1c77350741ac84d5bd7b87033fe22def0a1ce2
Yiran Li <yiran.li@hotmail.com>
2020-12-20 15:51:39 -0700

    Add elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+    <section>
+      <h1> How fast do you type?</h1>
+      <textarea />
+      <h4>Time remaining: </h4>
+      <button>START</button>
+      <h1>Word Count:</h1>
+    </section>
   );
 }
 
