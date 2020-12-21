46eb84df0e858fbac02dfecb110806e201e9c33f
Yiran Li <yiran.li@hotmail.com>
2020-12-20 21:06:26 -0700

    Add tests for render and word count

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+it('should render correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const title = screen.getByText('How fast do you type?');
+  const timeDisplay = screen.getByText('Time remaining: 10');
+  const startButton = screen.getByRole('button');
+  const wordCount = screen.getByText('Word Count: 0')
+
+  expect(timeDisplay).toBeInTheDocument();
+  expect(startButton).toBeInTheDocument();
+  expect(wordCount).toBeInTheDocument();
 });
+
+it('should count amount of words', async () => {
+  jest.useFakeTimers();
+  jest.runAllTimers();
+  render(<App />);
+
+  const startButton = screen.getByRole('button');
+  const textArea = screen.getByRole('textbox')
+  userEvent.click(startButton);
+  userEvent.type(textArea, 'This is an app to test type     speed');
+
+  await waitFor(() => expect(screen.getByText('Word Count: 8')).toBeInTheDocument())
+})
